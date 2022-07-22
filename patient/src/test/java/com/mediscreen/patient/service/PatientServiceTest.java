76c64f009f4275b810c1cab3cd4a6a658c8bd687
Pierrick Jadeau <jadeau.pierrick@gmail.com>
2022-07-22 22:43:37 +0200

    Ajout de jacoco et surefire dans les fichiers pom.xml

--- patient/src/test/java/com/mediscreen/patient/service/PatientServiceTest.java
+++ patient/src/test/java/com/mediscreen/patient/service/PatientServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -54,7 +53,7 @@ public class PatientServiceTest {
         patientService.addPatient(patient);
         List<PatientDTO> patients = patientService.getPatients();
 
-        assertThat(patients.get(5).getLastName()).isEqualTo("Garrix");
+        assertThat(patients.get(4).getLastName()).isEqualTo("Garrix");
     }
 
     @Test
@@ -71,21 +70,21 @@ public class PatientServiceTest {
     @Order(3)
     public void getPatients() {
         List<PatientDTO> patients = patientService.getPatients();
-        assertThat(patients.size()).isEqualTo(6);
+        assertThat(patients.size()).isEqualTo(5);
     }
 
     @Test
     @Order(4)
     public void getPatientById() throws PatientNotFoundException {
-        PatientDTO patientDTO = patientService.getPatientById(patient.getId());
-        assertThat(patientDTO.getId()).isEqualTo(6);
+        PatientDTO patientDTO = patientService.getPatientById(5L);
+        assertThat(patientDTO.getId()).isEqualTo(5);
     }
 
     @Test
     @Order(5)
     public void getPatientWithNullId() {
         try {
-            patientService.getPatientById(null);
+            patientService.getPatientById(10L);
         } catch (PatientNotFoundException e) {
             assertThat(e.getMessage()).contains("The patient with the id : " + 10 + " was not found");
         }
@@ -104,7 +103,7 @@ public class PatientServiceTest {
         try {
             patientService.getPatientByLastNameAndFirstNameAndBirthdate("Carment", patient.getLastName(), patient.getBirthdate());
         } catch (PatientNotFoundException e) {
-            assertThat(e.getMessage()).contains("The patient with these informations : " + "Carment" + " " + patient.getLastName() + " " + patient.getBirthdate() + " was not found");
+            assertThat(e.getMessage()).contains("The patient with these information : " + "Carment" + " " + patient.getLastName() + " " + patient.getBirthdate() + " was not found");
         }
     }
 
@@ -118,21 +117,21 @@ public class PatientServiceTest {
     @Test
     @Order(9)
     public void updatePatient() throws PatientNotFoundException {
-        PatientDTO patientDTO = patientService.getPatientById(patient.getId());
+        PatientDTO patientDTO = patientService.getPatientById(5L);
         Patient patient = patientMapper.fromDTO(patientDTO);
         patient.setAddress("6 St Sun");
 
         patientService.updatePatient(patient.getId(),patientMapper.toDTO(patient));
         List<PatientDTO> patients = patientService.getPatients();
 
-        assertThat(patients.get(5).getAddress()).isEqualTo("6 St Sun");
+        assertThat(patients.get(4).getAddress()).isEqualTo("6 St Sun");
     }
 
     @Test
     @Order(10)
     public void updatePatientWithNullId() {
         try {
-            patientService.updatePatient(null,patient);
+            patientService.updatePatient(10L,patient);
         } catch (PatientNotFoundException e) {
             assertThat(e.getMessage()).contains("The patient with the id : " + 10 + " was not found");
         }
@@ -141,17 +140,17 @@ public class PatientServiceTest {
     @Test
     @Order(11)
     public void deletePatient() throws PatientNotFoundException {
-        patientService.deletePatientById(patient.getId());
+        patientService.deletePatientById(5L);
         List<PatientDTO> patients = patientService.getPatients();
 
-        assertThat(patients.size()).isEqualTo(5);
+        assertThat(patients.size()).isEqualTo(4);
     }
 
     @Test
     @Order(12)
     public void deletePatientWithNullId() {
         try {
-            patientService.deletePatientById(null);
+            patientService.deletePatientById(10L);
         } catch (PatientNotFoundException e) {
             assertThat(e.getMessage()).contains("The patient with the id : " + 10 + " was not found");
         }
